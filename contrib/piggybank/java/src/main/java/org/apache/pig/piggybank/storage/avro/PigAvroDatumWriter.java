d4054d9a58a4263e19a764a3a470a037ab2024c1
Bill Graham <bill.graham@gmail.com>
2011-08-02 17:58:57 -0700

    first pass at pig_2201

--- contrib/piggybank/java/src/main/java/org/apache/pig/piggybank/storage/avro/PigAvroDatumWriter.java
+++ contrib/piggybank/java/src/main/java/org/apache/pig/piggybank/storage/avro/PigAvroDatumWriter.java
@@ -315,7 +315,7 @@ public class PigAvroDatumWriter extends GenericDatumWriter<Object> {
 
     }
 
-    private NullPointerException npe(NullPointerException e, String s) {
+    protected NullPointerException npe(NullPointerException e, String s) {
         NullPointerException result = new NullPointerException(e.getMessage()
                                         + s);
         result.initCause(e.getCause() == null ? e : e.getCause());
@@ -347,23 +347,6 @@ public class PigAvroDatumWriter extends GenericDatumWriter<Object> {
         } else
             throw new RuntimeException("Unsupported type fixed:" + datum.getClass());
 
-    }
-
-    /**
-     * Called by the implementation of {@link #writeRecord} to retrieve
-     * a record field value.
-     */
-    @Override
-    protected Object getField(Object record, String name, int pos) {
-        if (record instanceof Tuple) {
-            try {
-                return ((Tuple) record).get(pos);
-            } catch (ExecException e) {
-                e.printStackTrace();
-                throw new RuntimeException(e);
-            }
-        } else
-            throw new RuntimeException("Unsupported type in record:" + record.getClass());
     }
 
     /**
